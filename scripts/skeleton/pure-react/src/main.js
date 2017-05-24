df42ac42af2f3f3efcd6009f1269cf8cddaaccbe
SAPIENT\adala4 <adalal4@sapient.com>
2017-05-24 14:17:35 +0530

    removed less & sass files from skeleton

--- scripts/skeleton/pure-react/src/main.js
+++ scripts/skeleton/pure-react/src/main.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainComponent from './components/main-component.js';
 
-import './styles/main.less';
-
 ReactDOM.render(<MainComponent />, document.getElementById('app-root'));
