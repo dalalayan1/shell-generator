3f1b74a6c9aa285f0475f2e20e60f062dbb0540c
SAPIENT\asark4 <asarkar2@sapient.com>
2017-05-26 15:29:40 +0530

    Fixed issues

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import webpack from 'webpack';
 
-const render = require('../dist/output_SSR.js');
+const render = require('../dist/SSR.js');
 const app = express();
 console.log('ren ',render);
 app.get('/', render.default);
 
-const port = 3000;
+const port = 3001;
 app.listen(port);
 console.log(`Listening on port ${port}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-
+	
 module.exports = {
-  name: 'server',
-  target: 'node',
-  externals: nodeExternals(),  
-  entry: [
-    './app/SSR.js'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist/'),
-    filename: 'output_SSR.js'
-  },
-  module: {
-    loaders: [
-      	{
-			test:/.jsx?$/,
-			exclude:/node_modules/,
-			loader:'babel-loader',
-			query:{
-				presets: ["es2015", "react", "stage-0"]
-			}
-		}
-    ]
-  }
-}
+	  name: 'server',
+    target: 'node',
+    externals: nodeExternals(),  
+	  entry: [
+      './app/SSR.js'
+    ],
+	  output: {
+	    path: path.join(__dirname, '/dist'),
+	    filename: 'SSR.js',
+	    libraryTarget: 'commonjs2',
+	  },
+    module: {
+      loaders: [{
+        test:/.jsx?$/,
+        exclude:/node_modules/,
+        loader:'babel-loader',
+        query:{
+          presets: ["es2015", "react", "stage-0"]
+        }
+      }]
+    }
+};
