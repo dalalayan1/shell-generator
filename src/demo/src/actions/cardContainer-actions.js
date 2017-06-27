389492aaab302dfa9103db6bcb7deb65a220f750
Ayan Dalal <adala4@sapient.com>
2017-06-27 16:53:49 +0530

    removed src which was added by mistake

--- src/demo/src/actions/cardContainer-actions.js
+++
@@ -1,9 +0,0 @@
-import { cardSuccess } from './..constants/action-types';
-import catalogDetails from './../data/catalog.json';
-
-export const fetchCatelogCards = () => {
-    return {
-        type: cardSuccess,
-        payload: catalogDetails
-    }
-}
