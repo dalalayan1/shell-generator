5945e027edaa0346b6b0de3ae16ccda174ee6c18
sairamchappidi <sairam.chappidi@gmail.com>
2017-07-24 13:02:48 +0530

    exporting the url-loader module

--- scripts/webpack/webpack-configs/loaders/index.js
+++ scripts/webpack/webpack-configs/loaders/index.js
@@ -2,5 +2,5 @@ var babelLoader = require('./babel-loader.js');
 var jsonLoader = require('./json-loader.js');
 var lessLoader = require('./less-loader.js');
 var sassLoader = require('./sass-loader.js');
-
-module.exports = {babelLoader,jsonLoader,lessLoader,sassLoader};
+var urlLoader =  require('./url-loader.js');
+module.exports = {babelLoader, jsonLoader, lessLoader, sassLoader, urlLoader};
